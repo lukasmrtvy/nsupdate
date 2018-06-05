ccebfdc7e31971174657c87ed08da2e2d7b28c1e
Lukas M <lukas.mrtvy@gmail.com>
2018-06-05 10:22:14 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -f /key/nsupdate.key ] && [ ${DNS_SERVER+set} ] && [ ${HOST+set} ] && [ ${DOMAIN+set} ] && [ ${TTL+set} ] && [ ${IP+set} ] ;then
+
+echo "server ${DNS_SERVER} \
+update add ${HOST}.${DOMAIN} ${TTL} IN A ${IP} \
+send \
+update add ${REVERSE_IP} ${TTL} IN PTR ${HOST}.${DOMAIN} \
+send \
+quit \
+" | nsupdate -k /key/nsupdate.key
+else
+  echo "no valid params"
+  
+fi
+
